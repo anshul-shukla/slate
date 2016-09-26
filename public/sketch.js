9dc7a0e7455134255b13448c3046b90b74dded23
anshul-shukla <java.ansh@gmail.com>
2016-09-26 20:19:59 +0530

    added temp id

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,3 @@
-// ITP Networked Media, Fall 2014
-// https://github.com/shiffman/itp-networked-media
-// Daniel Shiffman
-
-// Keep track of our socket connection
 var socket;
 
 function setup() {
@@ -17,7 +12,7 @@ function setup() {
   //background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
+  socket = io.connect('http://172.20.44.243:3000');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = app.listen(process.env.PORT || 3000, listen);
 
 // This call back just tells us that the server has started
 function listen() {
-  var host = server.address().address;
+  var host = "http://172.20.44.243";//server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://' + host + ':' + port);
 }
